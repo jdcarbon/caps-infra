536d04530084c441084529870756d72141232530
jdcarbon <jd@carbonlab.io>
2025-11-20 15:38:35 -0700

    Add versioning and encrytion to S3 bucket

--- infra/main.tf
+++ infra/main.tf
@@ -14,3 +14,21 @@ resource "aws_s3_bucket" "site" {
         Env     = "dev"
     }
 }
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.site.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.site.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
